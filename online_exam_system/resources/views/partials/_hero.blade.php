eddbf401e17d733cdbb1f321a81fec1aa22c7583
LonG <bunlongly03@gmail.com>
2024-03-06 15:18:47 +0700

    Register, Login User

---
+++ online_exam_system/resources/views/partials/_hero.blade.php
@@ -0,0 +1,22 @@
+<section class="relative h-96 bg-laravel flex flex-col justify-center items-center text-center space-y-4 mb-4 overflow-hidden">
+    <!-- Background image with inline styles for full coverage -->
+    <div class="absolute top-0 left-0 w-full h-full opacity-10 background-cover bg-cover bg-center bg-no-repeat"
+        style="background-image: url('images/lim.building.jpeg');">
+    </div>
+
+    <!-- Content -->
+    <div class="z-10">
+        <h1 class="text-6xl font-bold uppercase text-white">
+            Limkokwing University
+        </h1>
+        <p class="text-2xl text-gray-200 font-bold my-4">
+            Get new experience with online exam system
+        </p>
+        <div>
+            <a href="register.html"
+               class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">
+                Contact Info
+            </a>
+        </div>
+    </div>
+</section>
--- online_exam_system/routes/web.php
+++ online_exam_system/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,20 @@ Route::get('/', function () {
     return view('listings');
 });
 
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+//Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+//Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+ 
+
+//Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+
+//Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
