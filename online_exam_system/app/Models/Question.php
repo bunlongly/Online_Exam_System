94c4fe19537924694642e5ff29b76b44ece9c696
LonG <bunlongly03@gmail.com>
2024-03-09 16:07:28 +0700

    push the modal to the database

---
+++ online_exam_system/app/Models/Question.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    // Define the fillable fields
+    protected $fillable = ['course', 'question', 'type', 'difficulty', 'score'];
+}
---
+++ online_exam_system/database/migrations/2024_03_09_090206_create_questions_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('course');
+            $table->text('question');
+            $table->string('type');
+            $table->string('difficulty');
+            $table->integer('score');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
